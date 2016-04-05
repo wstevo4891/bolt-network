bc10b852cd2cc4765f8c7aa5ee790d4800691da9
Eric Stephenson <ericb.stephenson@gmail.com>
2016-04-05 12:06:07 -0700

    add and remove genres in movie edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -75,6 +75,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:title, :photo, :summary, :rating, genre_ids: [])
+      params.require(:movie).permit(:title, :photo, :photo_cache, :summary, :rating, :genre_id, genre_ids: [])
     end
 end
