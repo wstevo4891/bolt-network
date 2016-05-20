c5893463c3da091185f5357e846f6c196eeed0ac
Eric Stephenson <ericb.stephenson@gmail.com>
2016-05-20 01:19:11 -0700

    changed access key method in asset_sync.rb

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,7 +1,7 @@
 if defined?(AssetSync)
 	AssetSync.configure do |config|
 		config.fog_provider = ENV['FOG_PROVIDER']
-		config.aws_access_key = ENV['AWS_ACCESS_KEY']
+		config.aws_access_key_id = ENV['AWS_ACCESS_KEY']
 		config.aws_secret_access_key = ENV['AWS_SECRET_KEY']
 		config.fog_directory = ENV['AWS_BUCKET']
 		config.fog_region = ENV['FOG_REGION']
