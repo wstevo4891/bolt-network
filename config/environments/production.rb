3abf3c722a886e7d3626d8a181531f7f14a569f1
Eric Stephenson <ericb.stephenson@gmail.com>
2016-06-08 08:50:36 -0700

    Use Environment variable for cloud front

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "d2kadijwaw2ik5.cloudfront.net"
+  config.action_controller.asset_host = ENV["CLOUD_FRONT"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
